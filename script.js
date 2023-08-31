f256be94f0f4fb69bd5f48e9c8da5df268c8cea1
RichardFront <richard.frontend@hotmail.com>
2023-08-31 05:26:15 -0300

    third commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//var hamburger = document.querySelector(".hamburger");
+
+document.querySelector(".hamburger").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
